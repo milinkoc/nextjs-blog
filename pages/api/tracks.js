a1ea71c4026d229168bf354b36d3980a1a9cbb49
milinkoc <milinko.corilic@gmail.com>
2022-03-20 21:46:14 +0100

    litle code refactoring on db access

--- pages/api/tracks.js
+++ pages/api/tracks.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import Database from 'better-sqlite3'
-const db = new Database(path.join(process.cwd(), 'database/Chinook.db'), {});
-const getTracksSql = `
+const db = new Database(path.join(process.cwd(), '/db/chinook.db'), {readonly: true});
+const sql = `
 select
 	t.TrackId,
 	t.Name,
@@ -20,13 +20,6 @@ from Track t
 	inner join Genre g on t.GenreId = g.GenreId`;
 
 export default function handler(req, res) {
-    // Get data from your database
-    let limit = req.query.limit;
-    
-    let sql = limit ? getTracksSql + ` limit ${limit}` : getTracksSql;
-    const trackData = db.prepare(sql).all();
-    //res.end( JSON.stringify(trackData));
-    //res.json(limit == null ? trackData : trackData.slice(0, limit));
-    res.status(200).json(trackData);
-    //res.status(200).json(users)
+    const tracks = req.query.limit ? db.prepare(sql + ` limit ?`).all(req.query.limit) : db.prepare(sql).all();
+    res.status(200).json(tracks);
   }
